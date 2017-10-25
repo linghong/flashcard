19dbba57205e521c766e5f59d7911d8f448678ab
linghong <linghong_chen@hotmail.com>
2017-10-25 18:26:14 -0400

    added Stack.test.js.

--- src/data/fixtures.js
+++ src/data/fixtures.js
@@ -2,9 +2,7 @@ export const stack = {
   id: 0,
   title: 'test title',
   cards: [
-    { id: 0, prompt: 'test prompt', answer: 'test answer' },
-    { id: 1, prompt: 'test prompt 2', answer: 'test answer 2' }
+    {id: 0, prompt:'test1', answer: 'answer1'},
+	{id: 1, prompt:'test2', answer: 'answer2'}
   ]
 };
-
-export const stacks = [stack];
