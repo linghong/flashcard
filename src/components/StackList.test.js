c68bf5cd45f733c2ab2d3d24cf8a862b48d8de37
linghong <linghong_chen@hotmail.com>
2017-10-28 00:33:27 -0400

    added snapshot test for StackList.

--- src/components/StackList.test.js
+++ src/components/StackList.test.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import { stacks } from '../data/fixtures';
 import { StackList } from './StackList';
@@ -11,4 +13,14 @@ describe('StackList', ()=>{
 	it('renders the correct number of the links', ()=>{
 		expect(stackList.find('Link').length).toEqual(props.stacks.length);
 	});
+});
+
+test('snapshot test for StackList', ()=>{
+	const component = renderer.create(
+		<MemoryRouter >
+			<StackList {...props} />
+		</MemoryRouter>
+	);
+	const tree=component.toJSON();
+	expect(tree).toMatchSnapshot();
 })
