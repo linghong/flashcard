f146991faa4d5ec25bb622158661aa6a73c71f49
linghong <linghong_chen@hotmail.com>
2017-10-29 23:56:15 -0400

    created http server.

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
-import config, { nodeEnv, logStars } from './config';
+import http from 'http';
 
-logStars('Function');
+const server = http.createServer();
+
+server.on('request', (req, res)=>{
+	res.write('Hello!\n');
+	setTimeout(()=>{
+		res.write('hello again\n');
+		res.end();
+	}, 3000);
+});
+
+server.listen(8080);
