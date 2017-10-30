7175516f7f12778c4cb90d7d5fbc6f28d067b105
linghong <linghong_chen@hotmail.com>
2017-10-30 11:31:50 -0400

    updated

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const config = require('./config');
 const express = require('express');
+const fs = require('fs');
 
 const server = express();
 
 server.get('/', (req, res)=>{
-	res.write('Hello!\n');
-	setTimeout(()=>{
-		res.write('hello again\n');
-		res.end();
-	}, 3000);
+	res.send('Hello!\n');
 });
+
+/*	
+//without using middle ware
+//The below code is the way to fetch file and display in the browser 
+fs.readFile('./about.html', (err, data)=>{
+		res.send(data.toString());
+	});
+});
+*/
+//using middleware, and move the about.html to the public folder.
+server.use(express.static('public'));
+
 
 server.listen(config.port, ()=>{
 	console.info('Express is listening on port ', config.port);
