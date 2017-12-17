b5a77fd4e513016d84447a55f5aeedcc6d9a85fa
linghong <linghong_chen@hotmail.com>
2017-12-17 01:31:08 -0500

    tried push again.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI, {useMongoClient: true});
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
