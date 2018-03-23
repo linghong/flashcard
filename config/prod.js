a0817b9538292702de7894a897dc4ba540811ce6
linghong <linghong_chen@hotmail.com>
2018-03-22 23:26:03 -0400

    end to end test: after logging in, the button will change to LOGOUT button.

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,6 @@ module.exports={
 	mongoURI: process.env.MONGO_URI,
 	cookieKey: process.env.COOKIE_KEY,
 	stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-	stripeSecretKey: process.env.STRIPE_SECRET_KEY
+	stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+	uesrTestId: process.env.USER_TEST_ID
 };
--- e2etests/header.test.js
+++ e2etests/header.test.js
@@ -12,7 +12,7 @@ beforeEach(async () => {
 });
 
 afterEach(async () =>{
-	await browser.close();
+	//await browser.close();
 });
 
 test('The text of the logo is correct', async ()=>{	
@@ -26,3 +26,33 @@ test('clicking login starts the OAuth2 flow', async () =>{
 	expect(url).toMatch(/accounts\.google\.com/);
 });
 
+test.only('when signed in, it will display logout button', async () =>{
+	const keys = require('../config/keys');
+
+	//session
+	const sessionObject = {
+		passport: {
+			user: keys.userTestId
+		}
+	};
+	const buffer = require('safe-buffer').Buffer;
+	const sessionString = Buffer.from(JSON.stringify(sessionObject))
+								.toString('base64');
+
+	//session.sig
+	const Keygrip = require('keygrip');
+	const keygrip = new Keygrip([keys.cookieKey]);
+	const sig = keygrip.sign('session=' + sessionString);
+
+	//set session and session.sig cookies
+	await page.setCookie({ name: 'session', value: sessionString });
+	await page.setCookie({ name: 'session.sig', value: sig });
+	//after set cookie, then need to refresh the browser
+	await page.goto('localhost:3000');
+
+	const text = await page.$eval('a[href="/api/logout"]', el =>el.innerHTML);
+	expect(text).toEqual('LOGOUT');
+});
+
+//session=eyJwYXNzcG9ydCI6eyJ1c2VyIjoiNWEzNWVlOGE1NDY1OTQ0YWJkZmU1NjQ5In19
+//session.sig=AGfnDpnNDG41qcOnLoXe6Ebw4LQ
