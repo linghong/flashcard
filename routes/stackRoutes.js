360130cb71ae941f6c450bbc9b80e44723a5a676
linghong <linghong_chen@hotmail.com>
2018-03-23 23:41:57 -0400

    created stack instance.

--- routes/stackRoutes.js
+++ routes/stackRoutes.js
@@ -1,7 +1,18 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+
+const Stack = mongoose.model('stacks');
+
 module.exports = app =>{
 	app.post('/api/stacks', requireLogin, requireCredits, (req, res) =>{
-		
+		const { title, cards }  = req.body;
+
+		const stack = new Stack({
+			title,
+			cards: cards.split(',').map((prompt, answer) =>{ prompt, answer }),
+			_user: req.user.id,
+			generated: Date.now()
+		});
 	});
 };
