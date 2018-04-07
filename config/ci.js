45b10cecf5b31e9f06202f990710342c0600fc03
linghong <linghong_chen@hotmail.com>
2018-04-07 00:20:12 -0400

    added ci.js.

---
+++ config/ci.js
@@ -0,0 +1,9 @@
+module.exports={
+	googleClientID: '436487394377-3im8vrtvjfuap05f1bepuc9peeas8u47.apps.googleusercontent.com',
+	googleClientSecret: 'UKub7Dos8kuf87yuXiiNpjoo',
+	mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',	
+	cookieKey: 'o67nhfdrfgogx;s;riikl2dg4fjhkAAqbh',
+	stripePublishableKey: 'pk_test_MVHSh3K62DVTNxDm4vtmdnzl',
+	stripeSecretKey: 'sk_test_lDTbtO1ajhe2wFqyRvTXZOHI',
+	redisUrl:'redis://127.0.0.1:6379'
+};}
