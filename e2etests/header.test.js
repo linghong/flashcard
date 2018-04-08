214bbed4308b6f6d14f797c1c7f3ad0d8dd1b8ab
linghong <linghong_chen@hotmail.com>
2018-04-08 18:47:42 -0400

    added login method to the CustomPage class.

--- e2etests/header.test.js
+++ e2etests/header.test.js
@@ -1,5 +1,3 @@
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
 const Page = require('./helpers/page');
 
 let page;
@@ -27,16 +25,8 @@ test('clicking login starts the OAuth2 flow', async () =>{
 });
 
 test('when signed in, it will display logout button', async () =>{
-	const start = Date.now();
-	const user = await userFactory();
-	const {session, sig } = sessionFactory(user);
-
-	//set session and session.sig cookies
-	await page.setCookie({ name: 'session', value: session });
-	await page.setCookie({ name: 'session.sig', value: sig });
-	//after set cookie, then need to refresh the browser
-	await page.goto('localhost:3000');
-	await page.waitFor('a[href="/api/logout"]');
-	const text = await page.$eval('a[href="/api/logout"]', el =>el.innerHTML);
-	expect(text).toEqual('LOGOUT');
+		await page.login();
+		
+		const text = await page.$eval('a[href="/api/logout"]', el =>el.innerHTML);
+		expect(text).toEqual('LOGOUT');
 });
--- e2etests/helpers/page.js
+++ e2etests/helpers/page.js
@@ -1,4 +1,6 @@
 const puppeteer =require('puppeteer');
+const sessionFactory = require('../factories/sessionFactory');
+const userFactory = require('../factories/userFactory');
 
 class CustomPage {
 	static async build (){
@@ -24,6 +26,19 @@ class CustomPage {
 	constructor(page){
 		this.page =page;
 	}
+
+	async login(){
+		const user = await userFactory();
+		const {session, sig } = sessionFactory(user);
+
+		//set session and session.sig cookies
+		await this.page.setCookie({ name: 'session', value: session });
+		await this.page.setCookie({ name: 'session.sig', value: sig });
+		//after set cookie, then need to refresh the browser
+		await this.page.goto('localhost:3000');
+		await this.page.waitFor('a[href="/api/logout"]');
+	}
+
 
 }
 
