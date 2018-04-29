58a2d1420d09add7d89e27206166232ca4c1829e
linghong <linghong_chen@hotmail.com>
2018-04-28 23:16:39 -0400

    added 'Add Avatar' button.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,21 +4,37 @@ import { connect } from 'react-redux';
 import Payment from './Payment';
 
 class Header extends Component {
+  state = {
+      avatar: null
+  };
+
   renderLog(){
     switch(this.props.auth){
       case null:
         return <li>'logging'</li>;
       case false:
-        return <li><a href="/auth/google">LOGIN WITH GOOGLE</a></li>;
+        return [<li><a href="/auth/google">LOGIN WITH GOOGLE</a></li>];
       default:
         return [
         <li key = "1"><Link to='/dashboard'>DASHBOARD</Link></li>,
-        <li key = "2"><Link to='/stackform'>ADD NEW CARD STACK</Link></li>,
+        <li key = "2"><Link to='/stackform'>ADD CARD STACK</Link></li>,
         <li key = "3">CREDITS: { this.props.auth.credits } </li>,
         <li key = "4" style ={{margin: '0 10px'}}><Payment/> </li>,
-        <li key = "5"><a href="/api/logout">LOGOUT</a></li>
+        <li key = "5"><a href="/api/logout">LOGOUT</a></li>,
+        <li key = "6">
+          {
+            this.state.avatar===null?<label>Add Avatar<input style={{display: 'none'}} type="file" accept="image/*" onChange={this.addAvatar} /></label>:<img src="" alt="avatar" />
+          }
+        </li>        
         ];
     }
+  }
+
+  addAvatar = (event)=>{
+    const avatarFile =event.target.files[0];
+    this.setState({
+      avatar: avatarFile
+    });
   }
 
 	render(){
